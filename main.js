7ed7eea0bbbac1bf87907341923ad10e7bc7ea6b
alderjunio <alder.junio@gmail.com>
2024-07-29 12:28:57 -0400

    update funcoes

---
+++ main.js
@@ -0,0 +1,5 @@
+import { mostraCidade, mostraIdade, mostraHobby } from "./funcoes.js";
+
+console.log(mostraIdade("Alder", 41));
+console.log(mostraCidade("Alder", "Boa Vista-RR"));
+console.log(mostraCidade("Alder", "Aprender Programação"));
